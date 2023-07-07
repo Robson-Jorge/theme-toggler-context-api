df0c2ff94d4e8cbd017e6a323ac32e46f5cde92a
Lucas Kaíque <lucas.kaique175@hotmail.com>
2023-07-07 19:31:37 -0300

    fixed usetheme hook

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -2,13 +2,25 @@ import { useState, useEffect } from 'react';
 
 export default function useTheme(key, initialTheme) {
 
-    const [ theme, setTheme ] = useState(() => {
+    const [theme, setTheme] = useState(() => {
         const storedTheme = localStorage.getItem(key);
-        return storedTheme ? JSON.parse(storedTheme) : initialTheme
+        if (storedTheme) {
+            try {
+                return JSON.parse(storedTheme);
+            } catch (error) {
+                console.error("Error parsing stored theme:", error);
+            }
+        }
+        // Use o tema inicial se nenhum tema armazenado ou em caso de erro
+        return initialTheme;
     })
 
     useEffect(() => {
-        localStorage.setItem(key , JSON.stringify(theme));
+        try {
+            localStorage.setItem(key , JSON.stringify(theme));
+        } catch (error) {
+            console.error("Error storing theme:", error);
+        }
     }, [key, theme]);
 
     return [theme, setTheme]
